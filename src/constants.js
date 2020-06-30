c15451eaca8884439f75b5a7cb068d44e68b46c0
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2020-06-30 15:30:25 +0300

    add constants

---
+++ src/constants.js
@@ -0,0 +1,26 @@
+export const genreTypes = {
+  tragedy: 'tragedy',
+  comedy: 'comedy',
+};
+
+export const audienceBasePricing = {
+  tragedy: 40000,
+  comedy: 30000,
+};
+
+export const audienceExtraPricng = {
+  tragedy: 1000,
+  comedy: 500,
+};
+
+export const audienceStaticExtraPricing = {
+  tragedy: 0,
+  comedy: 10000,
+};
+
+export const baseAudienceCount = {
+  tragedy: 30,
+  comedy: 20,
+};
+
+export const CREDITS_AUDIENCE_COUNT = 30;
